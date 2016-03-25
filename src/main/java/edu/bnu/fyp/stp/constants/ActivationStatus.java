f45fe94c19996da809cd98e1a37549dec81e21f6
Danish <iamdanish@live.co.uk>
2016-03-25 17:21:48 +0500

    Starting fresh, deleted everything to pull new files

--- src/main/java/edu/bnu/fyp/stp/constants/ActivationStatus.java
+++
@@ -1,8 +0,0 @@
-package edu.bnu.fyp.stp.constants;
-
-/**
- * Created by Abdul Basit on 3/16/2016.
- */
-public enum ActivationStatus {
-    Active, Inactive
-}
--- src/main/java/edu/bnu/fyp/stp/constants/RequirementsPriority.java
+++
@@ -1,8 +0,0 @@
-package edu.bnu.fyp.stp.constants;
-
-/**
- * Created by Abdul Basit on 3/2/2016.
- */
-public enum RequirementsPriority {
-    HIGH, LOW, MEDIUM
-}
